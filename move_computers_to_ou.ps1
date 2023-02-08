026b3e562f6d77e68621e92dd66aff2389f13f66
Frankie <34449029+usagibryan@users.noreply.github.com>
2023-02-08 08:06:58 -0500

    Delete move_computers_to_ou.ps1

--- move_computers_to_ou.ps1
+++
@@ -1,9 +0,0 @@
-﻿$computers = Get-Content C:\list_of_pcs.csv
-
-$TargetOU = "OU=roomumber,OU=computers,OU=department,DC=companyname,DC=com"
-
-ForEach($computer in $computers){
-
-Get-ADComputer $computer | Move-ADObject -TargetPath $TargetOU
-
-}
